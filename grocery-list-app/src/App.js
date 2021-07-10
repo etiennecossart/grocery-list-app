b8a49190986d033a16c3ec90e1cb3d144557cfc3
etiennecossart <41974406+etiennecossart@users.noreply.github.com>
2021-07-10 13:25:05 -0400

    next test

--- grocery-list-app/src/App.js
+++ grocery-list-app/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and get this started!
+          Learn React test test
         </a>
       </header>
     </div>
