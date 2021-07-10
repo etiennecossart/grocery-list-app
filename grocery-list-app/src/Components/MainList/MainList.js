3752abb53b93b188daccda5307369253d8ac117b
etiennecossart <41974406+etiennecossart@users.noreply.github.com>
2021-07-10 13:36:54 -0400

    very first list (vid - 6 challenge)

---
+++ grocery-list-app/src/Components/MainList/MainList.js
@@ -0,0 +1,17 @@
+import React from 'react'
+console.log('arrived at main List!')
+
+function MainList(){
+    return(
+        <div>
+            <h1>This is a list</h1>
+            <ul>
+                <li>Eggs</li>
+                <li>Butter</li>
+                <li>Carrots</li>
+            </ul>
+        </div>
+    )
+}
+
+export default MainList;
